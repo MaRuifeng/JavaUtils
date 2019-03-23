c771d6803c64da3e0d2f0c336ad7ead349064d3d
MaRuifeng <ruifeng.ma@thoughtworks.com>
2019-03-23 17:45:50 +0800

    Added Enum examples.

---
+++ src/enumeration/moreUseFullEnum/enumWithValues/EnumWithValues.java
@@ -0,0 +1,24 @@
+package enumeration.moreUseFullEnum.enumWithValues;
+
+/**
+ * See how Java enum with values works
+ *
+ * @author Ruifeng Ma
+ * @since 2019-Mar-16
+ */
+
+public class EnumWithValues {
+
+    public static void main(String[] args) {
+        for (Weekday day: Weekday.values()) {
+            System.out.println(day.getDaysGreeting());
+            if (day.isWeekend()) {
+                System.out.println("Oh! Happy " + day.name() + "!");
+            } else {
+                System.out.println("Snap! It's still " + day.name() + "...");
+            }
+            System.out.println();
+        }
+        System.out.println("All rabbits gone!");
+    }
+}
---
+++ src/enumeration/moreUseFullEnum/enumWithValues/Weekday.java
@@ -0,0 +1,41 @@
+package enumeration.moreUseFullEnum.enumWithValues;
+
+/**
+ * Instead of being defined as constant types, Java enum can be more useful with value members
+ * as if they would work like objects in JavaScript.
+ *
+ * https://dzone.com/articles/java-enums-how-to-make-much-more-useful
+ *
+ * @author Ruifeng Ma
+ * @Since 2019-Mar-16
+ */
+
+public enum Weekday {
+    SUNDAY("Sunday Funday", true),
+    MONDAY("Marketing Monday"),
+    TUESDAY("Trendy Tuesday"),
+    WEDNESDAY("Wellness Wednesday"),
+    THURSDAY("Thankful Thursday"),
+    FRIDAY("Foodie Friday"),
+    SATURDAY("Social Saturday", true);
+
+    private String daysGreeting;
+    private boolean isWeekend;
+
+    Weekday(final String greeting) {
+        this.daysGreeting = greeting;
+    }
+
+    Weekday(final String greeting, final boolean isWeekend) {
+        this(greeting);
+        this.isWeekend = isWeekend;
+    }
+
+    public String getDaysGreeting() {
+        return this.daysGreeting;
+    }
+
+    public boolean isWeekend() {
+        return this.isWeekend;
+    }
+}
